7d918d097a643117cdf019659538cd93610d0016
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-09 18:56:38 -0400

    refactor: implement lazy loading for Globe component and wrap it in Suspense for improved user experience

--- exercises/04.code-splitting/01.problem.lazy/src/index.tsx
+++ exercises/04.code-splitting/01.problem.lazy/src/index.tsx
@@ -1,19 +1,11 @@
-import { useState } from 'react'
+import { lazy, Suspense, useState } from 'react'
 import * as ReactDOM from 'react-dom/client'
 import './index.css'
-// 💣 remove this import
-import Globe from './globe.tsx'
 
-// 🐨 use lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the './globe' module.
+const Globe = lazy(() => import('./globe.tsx'))
 
 function App() {
 	const [showGlobe, setShowGlobe] = useState(false)
-
-	// 🐨 wrap the code below in a <Suspense /> component
-	// with a fallback.
-	// 💰 try putting it in a few different places and observe how that
-	// impacts the user experience.
 	return (
 		<div
 			style={{
@@ -34,8 +26,9 @@ function App() {
 				{' show globe'}
 			</label>
 			<div style={{ width: 400, height: 400 }}>
-				{/* 🐨 stick a Suspense boundary around this with a fallback of "loading..." */}
-				{showGlobe ? <Globe /> : null}
+				<Suspense fallback={'Loading...'}>
+					{showGlobe ? <Globe /> : null}
+				</Suspense>
 			</div>
 		</div>
 	)
