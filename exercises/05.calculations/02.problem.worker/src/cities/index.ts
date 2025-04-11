60e8fd900284e233ebfd6d6f6ea64c607376a604
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-11 13:09:41 -0400

    refactor: move city search functionality to a dedicated worker file and utilize Comlink for improved performance

--- exercises/05.calculations/02.problem.worker/src/cities/index.ts
+++ exercises/05.calculations/02.problem.worker/src/cities/index.ts
@@ -1,23 +1,13 @@
-// 🐨 move all this to a new file at ./filter-cities.worker.ts 👇
-import { matchSorter } from 'match-sorter'
-import cities from './us-cities.json'
+import * as Comlink from 'comlink'
+import { type Exposed } from './filter-cities.worker'
 
-const allCities = cities.map((city, index) => ({ ...city, id: String(index) }))
+const worker = new Worker(
+	new URL('./filter-cities.worker.ts', import.meta.url),
+	{ type: 'module' },
+)
 
-export function searchCities(filter: string) {
-	return matchSorter(allCities, filter, { keys: ['name'] })
+const filterCities = Comlink.wrap<Exposed>(worker)
+
+export async function searchCities(input: string) {
+	return filterCities.searchCities(input)
 }
-// 🐨 move all that to a new file at ./filter-cities.worker.ts 👆
-
-// 🐨 🛑 finish the instructions in ./filter-cities.worker.ts before continuing here...
-
-// 🐨 you're going to want this:
-// import * as Comlink from 'comlink'
-// 🐨 import the Exposed type from './fitler-cities.worker'
-
-// 🐨 create a new Worker object out of the ./filter-cities.worker.ts module
-
-// 🐨 create a filterCities object by calling Comlink.wrap with the worker
-// 🦺 you can use the Exposed type as the generic for some type safety
-
-// 🐨 export a new searchCities that calls the filterCities.searchCities API with a given input
