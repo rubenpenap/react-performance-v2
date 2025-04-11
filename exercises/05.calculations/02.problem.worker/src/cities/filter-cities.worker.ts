62a3fdd443b91b1065f2a11694486949c1fe85cb
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-11 13:09:35 -0400

    refactor: implement city filtering functionality in worker using Comlink for improved performance

--- exercises/05.calculations/02.problem.worker/src/cities/filter-cities.worker.ts
+++ exercises/05.calculations/02.problem.worker/src/cities/filter-cities.worker.ts
@@ -1,9 +1,17 @@
-// 🐨 you're gonna need this:
-// import * as Comlink from 'comlink'
-// 🐨 move the stuff that's in index.ts into this file.
+import * as Comlink from 'comlink'
+import { matchSorter } from 'match-sorter'
+import cities from './us-cities.json'
 
-// 🐨 create an object called "exposed" that has searchCities in it
-// 🐨 call Comlink.expose with the exposed object
-// 🦺 create and export a type called "Exposed" that is "typeof exposed"
-//   (you'll need this to get nice type safety in the main thread)
-// 💰 export type Exposed = typeof exposed
+const allCities = cities.map((city, index) => ({ ...city, id: String(index) }))
+
+export function searchCities(filter: string) {
+	return matchSorter(allCities, filter, { keys: ['name'] })
+}
+
+const exposed = {
+	searchCities,
+}
+
+Comlink.expose(exposed)
+
+export type Exposed = typeof exposed
